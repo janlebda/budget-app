325d2dd8496e03d6a0b263e17cdfce074156d392
dzony03 <janjakublebda@gmail.com>
2026-06-06 20:04:40 +0200

    issues

--- backend/src/test/java/pk/jl/pasir_lebda_jan/PasirLebdaJanApplicationTests.java
+++ backend/src/test/java/pk/jl/pasir_lebda_jan/PasirLebdaJanApplicationTests.java
@@ -2,8 +2,10 @@ package pk.jl.pasir_lebda_jan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PasirLebdaJanApplicationTests {
 
     @Test
